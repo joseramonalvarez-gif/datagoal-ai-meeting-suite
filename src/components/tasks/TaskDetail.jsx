e6bd7507c968c2c90a4748a8605ab9be1e558e7b
Jose Ramón Alvarez <consultoria@nevadaamurai.com>
2026-02-20 22:45:11 +0000

    File changes

--- src/components/tasks/TaskDetail.jsx
+++ src/components/tasks/TaskDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
+import { notifyTaskAssigned, notifyTaskMention } from "./taskNotifications";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
